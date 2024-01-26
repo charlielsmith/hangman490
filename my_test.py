a3f81cb078ced1bd2d6c4f4c50ca5797558241ca
Charlie Smith <charliesmith@charlies-air.home>
2024-01-26 12:14:59 +0000

    My first commit

---
+++ my_test.py
@@ -0,0 +1,9 @@
+# Define two variables
+variable1 = 5
+variable2 = 7
+
+# Multiply the variables
+result = variable1 * variable2
+
+# Print the result
+print("The product of", variable1, "and", variable2, "is:", result)
